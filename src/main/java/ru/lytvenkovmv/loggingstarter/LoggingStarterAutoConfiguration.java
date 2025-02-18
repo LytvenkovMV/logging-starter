be7b5fa679a7d686b1133ce7056456e4ee4db470
Home PC <example@mail.com>
2025-02-18 18:08:39 +0300

    initial commit

---
+++ src/main/java/ru/lytvenkovmv/loggingstarter/LoggingStarterAutoConfiguration.java
@@ -0,0 +1,8 @@
+package ru.lytvenkovmv.loggingstarter;
+
+public class LoggingStarterAutoConfiguration {
+
+    public static void printLn(String input) {
+        System.out.println(input);
+    }
+}
