a2978d3f7013510ef953a17ffb93228c702d5ee7
Home PC <example@mail.com>
2025-05-22 12:28:08 +0300

    HW-27: refactored test

--- src/test/java/ru/lytvenkovmv/loggingstarter/service/RequestBodyChainTest.java
+++ src/test/java/ru/lytvenkovmv/loggingstarter/service/RequestBodyChainTest.java
@@ -1,6 +1,7 @@
 package ru.lytvenkovmv.loggingstarter.service;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import net.minidev.json.JSONObject;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -13,7 +14,13 @@ class RequestBodyChainTest {
 
     @Test
     void test() {
-        String processedBody = chain.process("", chain);
+        JSONObject json = new JSONObject();
+        json.appendField("name", "John")
+                .appendField("surname", "Doe")
+                .appendField("age", 25)
+                .appendField("active", true);
+
+        String processedBody = chain.process(json.toString(), chain);
 
         System.out.println(processedBody);
     }
